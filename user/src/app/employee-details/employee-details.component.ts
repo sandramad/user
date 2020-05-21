f21784f2eb24344b936ec132f21de5ceb6e5f037
Sandra Madalińska-Piórkowska <sandra@netbiznes.com>
2020-05-21 21:30:30 +0200

    Edycja danych

--- user/src/app/employee-details/employee-details.component.ts
+++ user/src/app/employee-details/employee-details.component.ts
@@ -1,10 +1,10 @@
 import { Employee } from './../employee';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, ViewChild  } from '@angular/core';
 import { EmployeeService } from '../employee.service';
 import { map } from 'rxjs/operators';
 import {Observable} from 'rxjs';
 import { Router, ActivatedRoute } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-employee-details',
@@ -19,24 +19,23 @@ export class EmployeeDetailsComponent implements OnInit {
   constructor(private employeeService: EmployeeService,
               private router: Router,
               private route: ActivatedRoute) { }
+  @ViewChild('u', { static: false }) userForm: NgForm;
 
   private id: number;
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.id = +params['id'];
-      console.log(this.id);
     });
     this.loadData(this.id);
   }
-
   loadData(id: number) {
     this.employeeService.getEmployee(this.id)
       .pipe(
         map(employees => employees.data)
       ).subscribe(res => {
       this.employee = res;
-      console.log(this.employee);
     });
+
   }
 
   deleteEmployee(id: number) {
@@ -47,13 +46,10 @@ export class EmployeeDetailsComponent implements OnInit {
         },
         error => console.log(error));
   }
-  save() {
-    this.employeeService.updateEmployee(this.id, this.employ)
-      .subscribe(data => console.log(data), error => console.log(error));
+  onSubmit(form: NgForm) {
+    this.submitted = true;
+    this.employeeService.updateEmployee(this.id, form.value)
+      .subscribe(data => data, error => console.log(error));
     this.employ = new Employee();
   }
-
-  editEmployee() {
-    this.submitted = true;
-    this.save();
-  }}
+ }
